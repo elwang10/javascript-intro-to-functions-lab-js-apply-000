30d2c3b2c3f3555b659bb47362c1ace20c0d6e12
Eric Wang <eric.l.wang@blackrock.com>
2019-03-24 04:19:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string)  {
+  return string.toUpperCase()
+}
+
+function whisper(string)  {
+  return string.toLowerCase()
+}
+
+
+//const spy2 = logWhisper.spy()
+
+const spy = "hello"
+
+function logShout(string) {
+  //string = string.toUpperCase
+  //const spy = string
+  const test = string.toUpperCase
+  console.log(test)
+}
+
+const spy1 = "HELLO"
+
+function logWhisper(string) {
+  console.log(string)
+  
+}
+
+var lower = "I can't hear you!"
+var upper = "YES INDEED!"
+var mixed = "I love you, too."
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    return("I can't hear you!")
+  } else if (string.toUpperCase() === string) {
+    return("YES INDEED!")
+  } else {
+    return("I love you, too.")
+  }
+}
