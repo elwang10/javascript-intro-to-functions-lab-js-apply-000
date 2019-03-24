2fe202ba5118822442dfb83fdadbb835f13be17d
Eric Wang <eric.l.wang@blackrock.com>
2019-03-24 04:24:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const spy = "hello"
 function logShout(string) {
   //string = string.toUpperCase
   //const spy = string
-  const spy = console.log(string.toUpperCase)
+  console.log(string.toUpperCase)
 }
 
 
